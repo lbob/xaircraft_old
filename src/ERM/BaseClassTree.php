49fa407121f6a59fe9f6fb356f451717260bbd3c
lbob <skyweo@foxmail.com>
2015-01-13 10:26:29 +0800

    增加BaseClassTree辅助类

---
+++ src/ERM/BaseClassTree.php
@@ -0,0 +1,45 @@
+<?php
+namespace Xaircraft\ERM;
+
+/**
+ * Class BaseClassTree
+ *
+ * @author lbob created at 2015/1/13 10:09
+ */
+class BaseClassTree {
+
+    private $tableName;
+    private $classColumnName;
+    private $classNoLength;
+
+    public function __construct($tableName, $classColumnName, $classNoLength = 4)
+    {
+        $this->tableName = $tableName;
+        $this->classColumnName = $classColumnName;
+        $this->classNoLength = $classNoLength;
+    }
+
+    public function getNextClassNo($parentClassNo = '')
+    {
+        $lastClassNo = \Xaircraft\DB::table($this->tableName)
+            ->where($this->classColumnName, 'LIKE', "'" . $parentClassNo . "%'")
+            ->where('LENGTH(' . $this->classColumnName . ')', strlen($parentClassNo) + 4)
+            ->orderBy($this->classColumnName, 'DESC')
+            ->pluck($this->classColumnName)
+            ->execute();
+        $nextClassValue = 1;
+        if (isset($lastClassNo)) {
+            $nextClassValue = $lastClassNo + 1;
+        }
+        $nextClassNo = $nextClassValue . '';
+        $len = strlen($nextClassNo);
+        if ($len < $this->classNoLength) {
+            for ($i = 1; $i <= $this->classNoLength - $len; $i++) {
+                $nextClassNo = '0' . $nextClassNo;
+            }
+        }
+        return $parentClassNo . $nextClassNo;
+    }
+}
+
+ 

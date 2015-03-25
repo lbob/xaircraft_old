e967d25f75cac5cb5329b511de319e4bfff9a544
lbob <skyweo@foxmail.com>
2015-03-25 11:03:42 +0800

    为Command提供扩展

--- app/commands/TestCommand.php
+++ app/commands/TestCommand.php
@@ -9,7 +9,10 @@ class TestCommand extends \Xaircraft\Core\Cli\Command {
 
     public function execute()
     {
+        var_dump(\Xaircraft\DB::table('post')->select()->execute());
         var_dump($this->params);
+        \Xaircraft\Storage\Redis::getInstance()->set('test', 'test');
+        var_dump(\Xaircraft\Storage\Redis::getInstance()->get('test'));
         $this->showMessage('test');
     }
 }

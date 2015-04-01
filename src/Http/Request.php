99a425b0b7a0f939751d0151e33b50d51e0e89a0
lbob <skyweo@foxmail.com>
2015-04-01 20:04:42 +0800

    增加RequestFileInfo，增加对上传文件的解析

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -9,7 +9,8 @@ namespace Xaircraft\Http;
  * @package Xaircraft
  * @author lbob created at 2014/12/6 11:57
  */
-class Request {
+class Request
+{
 
     private $params = array();
 
@@ -85,12 +86,39 @@ class Request {
             $posts = array();
             $items = \Xaircraft\Common\Util::fast_array_key_filter($_POST, $prefix . '_');
             foreach ($items as $key => $value) {
-                $key = str_replace($prefix . '_', '', $key);
+                $key         = str_replace($prefix . '_', '', $key);
                 $posts[$key] = $value;
             }
             return $posts;
         } else {
             return $_POST;
+        }
+    }
+
+    /**
+     * 获取上传的文件信息
+     * @param null $keyword
+     * @return array
+     */
+    public function files($keyword = null)
+    {
+        if (isset($_FILES)) {
+            $files = array();
+            foreach ($_FILES as $formKeyword => $filesMeta) {
+                if (isset($key) && !strtolower($keyword) === strtolower($formKeyword)) {
+                    continue;
+                }
+                $fileInfo  = new RequestFileInfo();
+                $fileCount = count($filesMeta['name']);
+                for ($i = 0; $i < $fileCount; $i++) {
+                    $fileInfo->name     = $filesMeta['name'][$i];
+                    $fileInfo->type     = $filesMeta['type'][$i];
+                    $fileInfo->tmp_name = $filesMeta['tmp_name'][$i];
+                    $fileInfo->error    = $filesMeta['error'][$i];
+                    $fileInfo->size     = $filesMeta['size'][$i];
+                }
+                $files[] = $fileInfo;
+            }
         }
     }
 }
---
+++ src/Http/RequestFileInfo.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Xaircraft\Http;
+use Xaircraft\Common\IO;
+use Xaircraft\Mvc\BaseModel;
+
+
+/**
+ * Class RequestFileInfo
+ *
+ * @package Xaircraft\Http
+ * @author lbob created at 2015/4/1 19:49
+ */
+class RequestFileInfo extends BaseModel {
+
+    /**
+     * @var string
+     */
+    public $name;
+    /**
+     * @var string
+     */
+    public $type;
+    /**
+     * @var string
+     */
+    public $tmp_name;
+    /**
+     * @var integer
+     */
+    public $error;
+    /**
+     * @var integer
+     */
+    public $size;
+
+    /**
+     * 移动文件到指定路径
+     * @param $destinationPath
+     * @throws \Exception
+     * @return bool|mixed
+     */
+    public function moveUploadedFile($destinationPath)
+    {
+        if (!isset($destinationPath)) {
+            throw new \Exception("缺少目标文件路径");
+        }
+        if ($this->error == UPLOAD_ERR_OK) {
+            $destinationFolder = dirname($destinationPath);
+            IO::makeDir($destinationFolder);
+            return move_uploaded_file($this->tmp_name, $destinationPath);
+        }
+        throw new \Exception("文件[$this->name]上传错误，错误代码：" . $this->error);
+    }
+}
+
+ 
